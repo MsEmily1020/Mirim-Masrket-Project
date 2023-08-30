5bbb76e4b41559afe92104a84f8d46d19e614179
MsEmily1020 <peace10200khs@gmail.com>
2023-08-30 11:55:04 +0900

    ✨ images 파일 생성 및 DB 연결

--- src/setting/Setting.java
+++ src/setting/Setting.java
@@ -1,11 +1,23 @@
 package setting;
 
+import java.sql.DriverManager;
+
 public class Setting {
-	public Setting() {
+	public Setting() throws Exception {
+		var con = DriverManager.getConnection("jdbc:mysql://127.0.0.1/?serverTimezone=UTC", "root", "1234");
+		var stmt = con.createStatement();
+		
+		stmt.execute("set global local_infile = 1");
+		stmt.execute("drop schema if exists `mirim-market`");
+		stmt.execute("CREATE SCHEMA `mirim-market` DEFAULT CHARACTER SET utf8");
 		
 	}
 	
 	public static void main(String[] args) {
-		new Setting();
+		try {
+			new Setting();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 }
